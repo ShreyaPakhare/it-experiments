c79c34beffec9c49141103d5507432fdce275cf4
ShreyaPakhare <119624665+ShreyaPakhare@users.noreply.github.com>
2022-12-06 21:38:58 +0530

    modified footer and header

--- components/footer.js
+++ components/footer.js
@@ -2,7 +2,7 @@ function Footer() {
   return (
     <footer className="w-full h-[10vh] px-3 md:px-6 lg:px-9 font-montserrat grid place-items-center text-center border-t border-gray-200">
         <p className="text-sm font-medium">
-            &copy;2022 Copyright | All Rights Reserved | Developed by a Genius
+            &copy;2022 Copyright | Developed by ---
         </p>
     </footer>
   );
