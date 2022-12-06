de2a7fd3f4ae8e4eece47b51af988117de9c0ab6
ShreyaPakhare <119624665+ShreyaPakhare@users.noreply.github.com>
2022-12-06 21:36:13 +0530

    modified header

--- components/header.js
+++ components/header.js
@@ -4,7 +4,7 @@ import { RiTestTubeFill } from "react-icons/ri";
 function Header() {
   return (
     <nav className="w-full h-[10vh] px-3 md:px-6 lg:px-9 font-montserrat border-b border-gray-200 z-50">
-      <div className="h-full hidden md:flex justify-between items-center">
+      <div className="flex justify-between items-center">
         <Link href='/' passHref>
           <span className="font-bold text-2xl">
             IT Experiments
